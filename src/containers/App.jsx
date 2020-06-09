dce3dcacaabbbdfe3d543087b0efcd7b9f9d2048
kateMoscoso <katiemscs@gmail.com>
2020-06-09 23:53:02 +0200

    add hooks

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -5,38 +5,37 @@ import Carousel from "../components/Carousel";
 import CarouselItem from "../components/CarouselItem";
 import Categories from "../components/Categories";
 import Footer from "../components/Footer";
-
+import useInitialState from '../hooks/useInitialState'
 import "../assets/styles/App.scss";
+const API = 'http://localhost:3000/initalState'
+
 const App = () => {
-    const [ videos, setVideos ] = useState([])
-    const API = 'http://localhost:3000/initalState'
-    useEffect(() => {
-        fetch(API)
-        .then(response => response.json())
-        .then(data => setVideos(data))
-    },[])
+    const initialState = useInitialState(API)
     //se encarga de escuchar alguna propiedad que pueda cambiar y vuelve a ejecutarse
     //segunda propiedad para que no se quede el loop infinito
-  return videos.length === 0 ? <h1>Loading....</h1> : (
+  return initialState.length === 0 ? <h1>Loading....</h1> : (
     <div className="App">
       <Header />
       <Search />
-      {videos.mylist.length > 0 &&
+      {initialState.mylist.length > 0 &&
         <Categories title="Mi Lista">
           <Carousel>
+          {initialState.mylist.map(item =>
+            <CarouselItem key={item.id} {...item} />
+          )}
           </Carousel>
         </Categories>
       }
       <Categories title="Tendencias">
         <Carousel>
-          {videos.trends.map(item =>
+          {initialState.trends.map(item =>
             <CarouselItem key={item.id} {...item} />
           )}
         </Carousel>
       </Categories>
-      <Categories title="Originales de Platzi Video">
+      <Categories title="Videos">
         <Carousel>
-          {videos.originals.map(item =>
+          {initialState.originals.map(item =>
             <CarouselItem key={item.id} {...item} />
           )}
         </Carousel>
