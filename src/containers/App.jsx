36b53af2fbdf3b2b133a0dd6ff70c7390be14d5f
kateMoscoso <katiemscs@gmail.com>
2020-06-09 22:55:03 +0200

    condition

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,24 +1,49 @@
-import React from 'react'
-import Header from '../components/Header'
-import Search from '../components/Search'
-import Carousel from '../components/Carousel'
-import CarouselItem from '../components/CarouselItem'
-import Categories from '../components/Categories'
-import Footer from '../components/Footer'
+import React, { useEffect, useState } from "react";
+import Header from "../components/Header";
+import Search from "../components/Search";
+import Carousel from "../components/Carousel";
+import CarouselItem from "../components/CarouselItem";
+import Categories from "../components/Categories";
+import Footer from "../components/Footer";
 
-import '../assets/styles/App.scss'
-const App = () => (
+import "../assets/styles/App.scss";
+const App = () => {
+    const [ videos, setVideos ] = useState([])
+    const API = 'http://localhost:3000/initalState'
+    useEffect(() => {
+        fetch(API)
+        .then(response => response.json())
+        .then(data => setVideos(data))
+    },[])
+    //se encarga de escuchar alguna propiedad que pueda cambiar y vuelve a ejecutarse
+    //segunda propiedad para que no se quede el loop infinito
+  return (
     <div className="App">
-    <Header />
-    <Search />
-    <Categories title="Categorias" >
+      <Header />
+      <Search />
+      {videos.mylist.length > 0 &&
+        <Categories title="Mi Lista">
+          <Carousel>
+          </Carousel>
+        </Categories>
+      }
+      <Categories title="Tendencias">
         <Carousel>
-            <CarouselItem>
-
-            </CarouselItem>
+          {videos.trends.map(item =>
+            <CarouselItem key={item.id} {...item} />
+          )}
         </Carousel>
-    </Categories>
-    <Footer/>
+      </Categories>
+      <Categories title="Originales de Platzi Video">
+        <Carousel>
+          {videos.originals.map(item =>
+            <CarouselItem key={item.id} {...item} />
+          )}
+        </Carousel>
+      </Categories>
+      }
+      <Footer />
     </div>
-)
-export default App
+  );
+};
+export default App;
