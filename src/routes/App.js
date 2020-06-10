c08e6a0fe9cd878f6ceea909089ae983881f37bb
kateMoscoso <katiemscs@gmail.com>
2020-06-10 09:57:28 +0200

    fragment

--- src/routes/App.js
+++ src/routes/App.js
@@ -1,11 +1,18 @@
 import React from 'react'
-import { BrowserRouter, Route } from 'react-router-dom'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import Login from '../containers/Login'
+import Register from '../containers/Register'
 import Home from '../containers/Home'
+import NotFound from '../containers/NotFound'
+
 const App = () => (
     <BrowserRouter>
-      <Route exact path="/" component={Home}/>
-      <Route exact path="/login" component={Login}/>
+      <Switch>
+         <Route exact path="/" component={Home}/>
+         <Route exact path="/login" component={Login}/>
+         <Route exact path="/register" component={Register}/>
+         <Route component={NotFound}/>
+      </Switch>
     </BrowserRouter>
 )
 export default App
