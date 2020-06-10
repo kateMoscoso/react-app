eaed9adbb105cddef3109877b51b3812a853dc04
kateMoscoso <katiemscs@gmail.com>
2020-06-10 09:37:24 +0200

    Login route

--- src/routes/App.js
+++ src/routes/App.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import { BrowserRouter, Route } from 'react-router-dom'
+import Login from '../containers/Login'
 import Home from '../containers/Home'
 const App = () => (
     <BrowserRouter>
-    <Route exact path="/" component={Home}/>
+      <Route exact path="/" component={Home}/>
+      <Route exact path="/login" component={Login}/>
     </BrowserRouter>
 )
 export default App
