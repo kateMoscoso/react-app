d108e3c711e33395d901f15ba16eef44c4e50fcf
kateMoscoso <katiemscs@gmail.com>
2020-06-11 21:53:52 +0200

    player

--- src/containers/Player.jsx
+++ src/containers/Player.jsx
@@ -1,19 +1,37 @@
-import React from 'react'
-import "../assets/styles/components/Player.scss";
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
+import { getVideoSource } from '../actions';
+import '../assets/styles/components/Player.scss';
+import NotFound from '../containers/NotFound';
 
 const Player = props => {
-    return (
-        <div className="Player">
-            <video controls autoPlay>
-                <source src="" type="video/mp4" />
-            </video>
-            <div className="Player-back">
-                <button type="button" onClick={() =>props.history.goBack()}> 
-                  Regresar
-                </button>
-            </div>
-        </div>
-    )
+  const { id } = props.match.params;
+  const hasPlaying = Object.keys(props.playing).length > 0;
+  useEffect(() => {
+    props.getVideoSource(id);
+  }, []);
+  return hasPlaying ? (
+    <div className="Player">
+      <video controls autoPlay>
+        <source src={props.playing.source} type="video/mp4" />
+      </video>
+      <div className="Player-back">
+        <button type="button" onClick={() => props.history.goBack()}>
+          Regresar
+        </button>
+      </div>
+    </div>
+  ) : <NotFound />;
+};
+
+const mapStateToProps = state => {
+  return {
+    playing: state.playing,
+  }
 }
 
-export default Player
+const mapDispatchToProps = {
+  getVideoSource, 
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Player);
--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux'
-import { createStore } from 'redux' 
+import { createStore, compose } from 'redux' 
 import reducer from './reducers'
 import App from './routes/App';
 import { initalState } from '../initalState.json'
 
-console.log(initalState)
 const initialState = { ...initalState, user: {}, playing: {} }
 
-const store = createStore(reducer, initialState)
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose  
+  
+const store = createStore(reducer, initialState, composeEnhancers ()) 
 
 ReactDOM.render(
   <Provider store={store}>
